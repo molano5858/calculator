7b650e204799a858eca1cc2d1c9540137f9f3db4
molano5858 <molano5858@gmail.com>
2023-10-30 21:40:11 -0500

    starting with functionality

--- script.js
+++ script.js
@@ -2,8 +2,25 @@ const calculatorDisplay = document.querySelector("h1");
 const inputBtns = document.querySelectorAll("button");
 const clearBtn = document.getElementById("clear-btn");
 
+// add event listeners for numbers, operators and decimal
+inputBtns.forEach((inputBtn) => {
+  // only numbers doesn´t have any class so, we will select only numbers
+  if (inputBtn.classList.length == 0) {
+    inputBtn.addEventListener("click", () => {
+      sendNumberValue(inputBtn.value);
+    });
+  } else if (inputBtn.classList.contains("operator")) {
+    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains("decimal")) {
+    inputBtn.addEventListener("click", () => sendNumberValue(inputBtn.value));
+  }
+});
+
 function sendNumberValue(number) {
-  console.log(number);
+  // if current display value is 0 replace it if not add the number
+  const displayValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent =
+    displayValue === "0" ? number : displayValue + number;
 }
 
-sendNumberValue();
+console.log("hi");
